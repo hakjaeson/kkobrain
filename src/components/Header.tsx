0d222bd34197dc9cee0e5b0242e495b92e3cf2b6
hakjaeson <hakjae.son@gmail.com>
2024-03-26 10:46:26 +0900

    feat : trans react

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,41 +2,56 @@ import "../css/header/header.css";
 import "../css/header/mbmenu.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Autoplay } from "swiper/modules";
+import SwiperInit from "swiper";
 
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/autoplay";
-import { useEffect, useRef, useState } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 
 export const Header = () => {
+  // 스크롤 상태 여부
+  const [isScroll, setIsScroll] = useState<boolean>(false);
+
   // 모바일 메뉴 참조
-  const iconMore = useRef(null);
-  const mbMenu = useRef(null);
-  const mbInner = useRef(null);
+  const iconMore = useRef<HTMLAnchorElement | null>(null);
+  const mbMenu = useRef<HTMLDivElement | null>(null);
+  const mbInner = useRef<HTMLDivElement | null>(null);
   let isClick = true;
   // 모바일 메뉴 보이기 숨기를 적용
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   // useEffect 에서 태그를 찾지 않고 useRef 를 활용
-  const swLogTag = useRef(null);
+  const swLogTag = useRef<HTMLAnchorElement | null>(null);
   // swiper 를 위한 useRef
-  const swHeaderLogo = useRef(null);
+  const swHeaderLogo = useRef<SwiperInit | null>(null);
+
+  const swiperOption = {
+    modules: [EffectFade, Autoplay],
+    effect: "fade",
+    speed: 500,
+    autoplay: {
+      delay: 200,
+      disableOnInteraction: false,
+    },
+    onInit: (swiper: SwiperInit | null) => {
+      // useRef 를 이용해서 Swiper 를 저장한다.
+      swHeaderLogo.current = swiper;
+      // console.log("상단 슬라이드", swHeaderLogo.current);
+      swHeaderLogo.current?.autoplay.stop();
+    },
+  };
 
   const handleMouseEnter = () => {
-    if (swHeaderLogo.current.swiper) {
-      swHeaderLogo.current.swiper.autoplay.start();
-    }
+    swHeaderLogo.current?.autoplay.start();
   };
   const handleMouseLeave = () => {
-    if (swHeaderLogo.current.swiper) {
-      swHeaderLogo.current.swiper.autoplay.stop();
-      // // 첫 번째 슬라이드로 강제로 이동시킨다.
-      swHeaderLogo.current.swiper.slideTo(1);
-    }
+    swHeaderLogo.current?.autoplay.stop();
+    swHeaderLogo.current?.slideTo(1);
   };
 
   // 반응형 메뉴
-  const hanldeClickMore = (event) => {
+  const hanldeClickMore = (event: MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     if (isClick === false) {
       return;
@@ -58,26 +73,43 @@ export const Header = () => {
   };
 
   useEffect(() => {
-    // console.log(swHeaderLogo.current.swiper.autoplay);
-    swHeaderLogo.current.swiper.autoplay.stop();
-
     window.addEventListener("resize", function () {
       if (window.innerWidth >= 1024) {
         setIsOpen(false);
       }
     });
 
+    // 새로고침시 처리
+    let scrollPositionY = window.scrollY;
+    if (scrollPositionY > 0) {
+      setIsScroll(true); // 스크롤바가 이동했다.
+    } else {
+      setIsScroll(false); // 스크롤바가 상단 위치했다.
+    }
+
+    // 스크롤이 되는 경우 체크
+    window.addEventListener("scroll", function () {
+      let scrollPositionY = window.scrollY;
+      // console.log("스크롤", scrollPositionY);
+      if (scrollPositionY > 0) {
+        setIsScroll(true); // 스크롤바가 이동했다.
+      } else {
+        setIsScroll(false); // 스크롤바가 상단 위치했다.
+      }
+    });
+
     return () => {
-      window.removeEventListener("resize");
+      window.removeEventListener("resize", function () {});
+      window.removeEventListener("scroll", function () {});
     };
   }, []);
+
   return (
     <>
-      <header className="header">
+      <header className={isScroll ? "header header-b-line" : "header"}>
         <div className="inner space-between align-items-center">
           <a
             href="#"
-            alt="카카오브레인 블로그"
             aria-label="카카오브레인 블로그"
             className="logo space-between"
             ref={swLogTag}
@@ -88,19 +120,13 @@ export const Header = () => {
               handleMouseLeave();
             }}
           >
-            <img src="images/etc/logo-kakao.png" alt="카카오브레인" className="logo-kakao" />
+            <img
+              src="images/etc/logo-kakao.png"
+              alt="카카오브레인"
+              className="logo-kakao"
+            />
             {/* <!-- Start 로고 슬라이더 --> */}
-            <Swiper
-              className="swLogo"
-              ref={swHeaderLogo}
-              modules={[EffectFade, Autoplay]}
-              effect="fade"
-              speed={500}
-              autoplay={{
-                delay: 200,
-                disableOnInteraction: false,
-              }}
-            >
+            <Swiper className="swLogo" {...swiperOption}>
               <SwiperSlide>
                 <img src="./images/etc/logo-blog01.png" />
               </SwiperSlide>
@@ -134,33 +160,38 @@ export const Header = () => {
 
           <ul className="nav">
             <li>
-              <a href="#" alt="카카오브레인 소식" aria-label="카카오브레인 소식">
+              <a href="#" aria-label="카카오브레인 소식">
                 소식
               </a>
             </li>
             <li>
-              <a href="#" alt="카카오브레인 팀 & 크루" aria-label="카카오브레인 팀 & 크루">
+              <a href="#" aria-label="카카오브레인 팀 & 크루">
                 팀 & 크루
               </a>
             </li>
             <li>
-              <a href="#" alt="카카오브레인 영입" aria-label="카카오브레인 영입">
+              <a href="#" aria-label="카카오브레인 영입">
                 영입
               </a>
             </li>
             <li className="mb-search">
-              <a href="#" alt="카카오브레인 검색" aria-label="카카오브레인 검색" className="icon-bt icon-search">
+              <a
+                href="#"
+                aria-label="카카오브레인 검색"
+                className="icon-bt icon-search"
+              >
                 검색
               </a>
             </li>
             <li className="mb-more">
               <a
                 href="#"
-                alt="카카오브레인 반응형메뉴"
                 aria-label="카카오브레인 반응형메뉴"
-                className={isOpen ? "icon-bt icon-more active" : "icon-bt icon-more"}
+                className={
+                  isOpen ? "icon-bt icon-more active" : "icon-bt icon-more"
+                }
                 ref={iconMore}
-                onClick={(event) => {
+                onClick={event => {
                   hanldeClickMore(event);
                 }}
               >
@@ -171,20 +202,20 @@ export const Header = () => {
         </div>
       </header>
       <div className={isOpen ? "mb-menu active" : "mb-menu"} ref={mbMenu}>
-        <div className="mb-inner" ref={mbInner}>
+        <div className={isOpen ? "mb-inner active" : "mb-inner"} ref={mbInner}>
           <ul className="mb-nav">
             <li>
-              <a href="#" alt="카카오브레인 소식" aria-label="카카오브레인 소식">
+              <a href="#" aria-label="카카오브레인 소식">
                 소식
               </a>
             </li>
             <li>
-              <a href="#" alt="카카오브레인 팀 & 크루" aria-label="카카오브레인 팀 & 크루">
+              <a href="#" aria-label="카카오브레인 팀 & 크루">
                 팀 & 크루
               </a>
             </li>
             <li>
-              <a href="#" alt="카카오브레인 영입" aria-label="카카오브레인 영입">
+              <a href="#" aria-label="카카오브레인 영입">
                 영입
               </a>
             </li>
